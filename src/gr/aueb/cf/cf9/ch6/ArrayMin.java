a2507828db9e10ca00f7300546d5df74a66c6859
a8anassis <a8anassis@gmail.com>
2025-11-11 17:51:46 +0200

    feat: add array min

---
+++ src/gr/aueb/cf/cf9/ch6/ArrayMin.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.cf9.ch6;
+
+/**
+ * Βρίσκει το ελάχιστο στοιχείο ενός πίνακα.
+ */
+public class ArrayMin {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int getMinPosition(int[] arr) {
+        if (arr == null || arr.length == 0) return -1;
+
+        int minPosition = 0;
+        int minValue = arr[minPosition];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < minValue) {            // σύγκριση
+                minPosition = i;
+                minValue = arr[i];
+            }
+        }
+
+        return minPosition;
+    }
+}
