c36274f7a9a41918f2caed44619ca8b60c6953d7
a8anassis <a8anassis@gmail.com>
2025-11-11 18:40:11 +0200

    feat: add array min max with two algorithms

--- src/gr/aueb/cf/cf9/ch6/ArrayMin.java
+++ src/gr/aueb/cf/cf9/ch6/ArrayMin.java
@@ -24,4 +24,50 @@ public class ArrayMin {
 
         return minPosition;
     }
+
+    public static int getMinPosition2(int[] arr) {
+        if (arr == null || arr.length < 1) return -1;
+
+        int minPosition = 0;
+        int minValue = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minValue) {
+                minPosition = i;
+                minValue = arr[i];
+            }
+        }
+        return minPosition;
+    }
+
+    public static int getMaxPosition(int[] arr) {
+        if (arr == null || arr.length == 0) return -1;
+
+        int maxPosition = 0;
+        int maxValue = arr[maxPosition];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxValue) {            // σύγκριση
+                maxPosition = i;
+                maxValue = arr[i];
+            }
+        }
+
+        return maxPosition;
+    }
+
+    public static int getMaxPosition2(int[] arr) {
+        if (arr == null || arr.length < 1) return -1;
+
+        int maxPosition = 0;
+        int maxValue = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxValue) {
+                maxPosition = i;
+                maxValue = arr[i];
+            }
+        }
+        return maxPosition;
+    }
 }
