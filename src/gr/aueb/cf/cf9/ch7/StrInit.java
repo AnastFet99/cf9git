23232246ddd77c33bd45e198917c32c5a06e6999
a8anassis <a8anassis@gmail.com>
2025-11-18 20:32:39 +0200

    fix: add str length 0 is str empty

--- src/gr/aueb/cf/cf9/ch7/StrInit.java
+++ src/gr/aueb/cf/cf9/ch7/StrInit.java
@@ -9,6 +9,13 @@ public class StrInit {
     public static void main(String[] args) {
         String s1 = "Athens";                // popular
         String s2 = new String("Athens");   // not popular
+        String s3 = "";
+        String s4 = null;
+        boolean isEmpty;
+
+        // isEmpty = s3.length() == 0;
+        isEmpty = s3.isEmpty();
+
 
         System.out.println(s1);
         System.out.println("Length: " + s1.length());
