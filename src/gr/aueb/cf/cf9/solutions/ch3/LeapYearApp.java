8fa94cc070fd6ba831eafedd450e97e08b1fc3b5
a8anassis <a8anassis@gmail.com>
2025-11-22 14:04:46 +0200

    fix: change the algorithm of leap year. if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) { isLeap = true;

--- src/gr/aueb/cf/cf9/solutions/ch3/LeapYearApp.java
+++ src/gr/aueb/cf/cf9/solutions/ch3/LeapYearApp.java
@@ -3,8 +3,9 @@ package gr.aueb.cf.cf9.solutions.ch3;
 import java.util.Scanner;
 
 /** Ελέγχει αν ένα έτος είναι δίσεκτο.
- * Δίσεκτο είναι ένα έτος αν διαιρείται με το 4
- * και είτε δε διαιρείται με το 100 ή διαιρείται με το 100 και το 400.
+ *  Δίσεκτο είναι ένα έτος αν
+ *  Διαιρείται με 400, Ή
+ *  διαιρείται με το 4 ΚΑΙ όχι με το 100
  */
 public class LeapYearApp {
 
@@ -16,11 +17,11 @@ public class LeapYearApp {
         System.out.println("Please insert the year");
         year = in.nextInt();
 
-        /*if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+        /*if (year % 400 == 0 || (year % 4 == 0 && year % 100 !                                                                                                                 = 0)) {
             isLeap = true;
         }*/
 
-        isLeap = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        isLeap = year % 400 == 0 || (year % 4 != 0 && year % 100 != 0);
 
         System.out.println("Year: " + year + " is leap: " + isLeap);
     }
