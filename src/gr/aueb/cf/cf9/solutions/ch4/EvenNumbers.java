4abb5790aa00fb9aaf154f6270034ae1ab0db22d
a8anassis <a8anassis@gmail.com>
2025-11-24 19:42:50 +0200

    fix: add blank line

--- src/gr/aueb/cf/cf9/solutions/ch4/EvenNumbers.java
+++ src/gr/aueb/cf/cf9/solutions/ch4/EvenNumbers.java
@@ -1,6 +1,7 @@
 package gr.aueb.cf.cf9.solutions.ch4;
 
 public class EvenNumbers {
+
     public static void main(String[] args) {
         System.out.println("Οι άρτιοι από 1 έως 20:");
         for (int i = 1; i <= 20; i++) {
