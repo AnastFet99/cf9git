e18e510eb6275069fdedc4fb2b41b74bd9525cf4
a8anassis <a8anassis@gmail.com>
2026-01-13 19:13:20 +0200

    chore: change input string

--- src/gr/aueb/cf/cf9/ch19/LowerCaseAssert.java
+++ src/gr/aueb/cf/cf9/ch19/LowerCaseAssert.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class LowerCaseAssert {
 
     public static void main(String[] args) {
-        String s = "XAGDSDRt";    // τουλάχιστον 8 χαρακτήρες τουλάχιστον 1 lower case
+        String s = "XAGDSEEEEEEEEEEEDRt";    // τουλάχιστον 8 χαρακτήρες τουλάχιστον 1 lower case
 
         Pattern pattern = Pattern.compile("(?=.*?[a-z])^.{8,}$");  // Zero-length assertion
         Matcher matcher = pattern.matcher(s);
